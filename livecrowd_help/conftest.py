79f60bd903289837aaf6aea368a27eed4ad3bab6
Devlin Kros <devlin.kros.dk@gmail.com>
2025-01-31 09:45:48 +0100

    first commit

---
+++ livecrowd_help/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from livecrowd_help.users.models import User
+from livecrowd_help.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
