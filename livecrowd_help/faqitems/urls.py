eec54e79293d1b196484eda02593124adf0e1078
Devlin Kros <devlin.kros.dk@gmail.com>
2025-03-14 15:13:18 +0100

    added archived functionality and favorite events

--- livecrowd_help/faqitems/urls.py
+++ livecrowd_help/faqitems/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import dashboard_view, faq_edit_or_create_view, load_remaining_faqitems
-from livecrowd_help.faqitems.views import autocomplete, faq_delete_view
+from livecrowd_help.faqitems.views import autocomplete, faq_delete_view, toggle_archive_faq_item, toggle_favorite_event
 
 
 urlpatterns = [
@@ -10,4 +10,6 @@ urlpatterns = [
 	path('faq/<int:pk>/delete/', faq_delete_view, name='faq_delete'),
 	path('autocomplete/', autocomplete, name='autocomplete'),
 	path('load_remaining_faqitems/', load_remaining_faqitems, name='load_remaining_faqitems'),
+	path('faq/<int:pk>/archive', toggle_archive_faq_item, name='toggle_archive_faq_item'),
+	path('event/<int:event_id>/favorite', toggle_favorite_event, name='toggle_favorite_event')
 ]
